a97765aeab0f68e05cd15e83f363f1220f3880b9
dinithprimal <dinithprimal@gmail.com>
2023-09-20 10:36:18 +0530

    20/09/2023 10:36

---
+++ UnitStepFunction.m
@@ -0,0 +1,16 @@
+clc
+clear all
+close all
+
+n = -10:20;
+u = [zeros(1,10) 1 ones(1, 20)];
+
+figure(1)
+plot(n, u);
+hold on;
+stem(n, u)
+xlabel('Time/Discrete Intervals');
+ylabel('Unit Step Function');
+ylim([-0.1 1.1])
+grid on
+
